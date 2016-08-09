640ddbb1d50ee069c76f6ac02b38ac937e71b2ff
osnth01 <osnth01@gmail.com>
2016-08-09 18:34:55 -0400

    setup router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello!')
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+
+const App = () => <h1>Hello</h1>
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+  </Router>),
+  document.getElementById('root'))
